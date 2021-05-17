5b87260a8b7b8ad50177ce620eea8a1ad067072a
Ryan Lewis <rlewis94@gmail.com>
2021-05-16 18:47:27 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48bc7dd040ad35528064456ebe6301a3.js"
+  "/precache-manifest.7a72554c8b925e2c08815ddc7659197d.js"
 );
 
 self.addEventListener('message', (event) => {
